baa859faedaab7ba930f08a5bc800516d0074ad1
HoudaSebti <sebti.houda14@gmail.com>
2019-02-18 22:22:33 +0100

    remove

--- cmake/modules/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ETHIY/Workspace/Projects/houda/cra_pricer/src/lib
-# Build directory: /Users/ETHIY/Workspace/Projects/houda/cra_pricer/cmake/modules/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
