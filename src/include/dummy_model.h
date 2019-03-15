a92e52026096bf6ced152219ea48a304d5cbfc56
HoudaSebti <sebti.houda14@gmail.com>
2019-03-15 12:08:02 +0100

    dummyModel class now implements generatePath method

--- src/include/dummy_model.h
+++ src/include/dummy_model.h
@@ -9,5 +9,10 @@ class DummyModel : Models<double>{
         ~DummyModel();
     private:
         double simulateValue(int daysFromStartDate) const override;
+        Path<double> generatePath(
+            ql::Date const& startDate,
+            ql::Date const& endDate,
+            ql::Calendar const& calendar
+        ) const override;
         void calibrate(std::vector<std::istream> const& data) override;
 };
--- src/lib/dummy_model.cpp
+++ src/lib/dummy_model.cpp
@@ -5,6 +5,20 @@ DummyModel::~DummyModel(){};
 double DummyModel::simulateValue(int daysFromStartDate) const{
         return 0.0;
     }
+Path<double> DummyModel::generatePath(
+    ql::Date const& startDate,
+    ql::Date const& endDate,
+    ql::Calendar const& calendar
+) const{
+    Path<double> path(
+        startDate,
+        endDate,
+        calendar
+    );
+    for(auto t = 0; t < path.getSize(); ++t)
+        path.addElement(DummyModel::simulateValue(t));
+    return path;
+}
 void DummyModel::calibrate(std::vector<std::istream> const& data){
 
     }
